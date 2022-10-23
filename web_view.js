24d199af435ba88c37d1e510dce8c89c16512aca
Raphael Qureshi <darkslider345@gmail.com>
2022-10-23 11:08:00 +0500

    Add files via upload

---
+++ web_view.js
@@ -0,0 +1,10 @@
+if (Java.available) {
+	Java.perform(function() {
+		Java.scheduleOnMainThread(function() {
+			var WebView = Java.use("android.webkit.WebView");
+			WebView.setWebContentsDebuggingEnabled(true);
+			console.log(WebView);
+			console.log("WebView debug enabled successfully!");
+		});			
+	});
+}
